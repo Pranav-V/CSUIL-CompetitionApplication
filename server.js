a882cfd98d1ea9ce80d92f16a7767bb4796e540c
Pranav <pvenkatesh3141@gmail.com>
2021-04-23 16:17:13 -0500

    adsf

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const path = require("path")
-
-require("dotenv").config()
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const path = require('path')
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000
-
-app.use(cors());
-app.use(express.json());
+console.log("here")
+app.use(cors())
+app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'client/build')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build'))
 })
-
-mongoose.connect("mongodb+srv://pnav:pdatabase@cluster0.jvhte.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true})
-
+console.log('here')
+mongoose.connect("mongodb+srv://pnav:pdatabase@cluster0.jvhte.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
 const connection = mongoose.connection
-
 connection.once('open', () => {
-    console.log("MongoDB connection established")
+    console.log('MongoDB connection established')
 })
-
+/*
 const userRouter = require('./routes/user')
 const answerRouter = require('./routes/answer')
 const adminRouter = require('./routes/admin')
 app.use('/users',userRouter)
 app.use('/answer',answerRouter)
 app.use('/admin',adminRouter)
-
+*/
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
