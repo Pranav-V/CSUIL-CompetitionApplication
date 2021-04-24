4869b4b8662b13ab801f8f9139fd884a3fcb488c
Pranav <pvenkatesh3141@gmail.com>
2021-04-24 16:08:40 -0500

    adsf

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const port = process.env.PORT || 5000
 console.log("here")
 app.use(cors())
 app.use(express.json())
-/*
+
 app.use(express.static(path.join(__dirname, 'client/build/')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/'))
 })
-*/
+
 console.log('here')
 mongoose.connect("mongodb+srv://pnav:pdatabase@cluster0.jvhte.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
 const connection = mongoose.connection
