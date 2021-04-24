926aeae879b58a4a1e7408e412f0dff4b867a6ba
Pranav <pvenkatesh3141@gmail.com>
2021-04-24 17:58:21 -0500

    asdf

--- client/src/components/WrittenResponse.js
+++ client/src/components/WrittenResponse.js
@@ -1,79 +1,24 @@
-import React, {useState,useEffect} from "react"
-import {useCookies} from "react-cookie"
-import axios from "axios"
-import {useHistory} from "react-router-dom"
-import NavBar from "./NavBar"
+import React from "react";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css"; //Example style, you can use another
 
-export default function WrittenResponse()
-{
-    const [recentImage,setRecentImage] = useState({})
-    const [caption, setCaption] = useState("")
-    const [uploadedImageUrl,setuploadedImageUrl] = useState("")
-    const [uploadedImage,setuploadedImage] = useState({})
-    const [imageList, setImageList] = useState([])
-
-    useEffect(() => {
-        console.log("here")
-        axios.post('/image/getImages/')
-            .then(response => {
-                setImageList(response.data.images);
-            })
-            .catch(err => alert(err));
-        console.log('here')
-    },[])
-
-    function uploadImage() 
-    {
-        if (!caption.trim() || !uploadedImage.name) {
-            return alert('Caption or file is missing');
-        }
-
-        let formData = new FormData();
-        formData.append('caption', caption);
-        formData.append('file',uploadedImage);
-
-        axios.post('/image/', formData)
-            .then((response) => {
-                response.data.success ? alert('File successfully uploaded') : alert('File already exists');
-            })
-            .catch(err => alert('Error: ' + err));
-    }
-    return ( 
-        <div>
-            <input
-                type="text"
-                placeholder="Enter caption..."
-                onChange={event => setCaption(event.target.value )}
-                value={caption}
-            />
-            <input
-                type="file"
-                className="Upload__Input"
-                onChange={(event) => {
-                    setuploadedImageUrl(URL.createObjectURL(event.target.files[0]))
-                    setuploadedImage(event.target.files[0])   
-                }}
-            />
-            <button onClick={uploadImage} className="Upload__Button">Upload</button>
-            {
-                        (imageList!=[])?
-                        <div className="ListImageContainer">
-                        {imageList.map((file) => (
-                            <div className="ListImage">
-                                <p className="ListImage__Caption">{file.caption}</p>
-                                <p className="ListImage__Date">{file.createdAt}</p>
-                                <img
-                                    src={'/image/image/' + file.filename}
-                                    alt="list-image"
-                                    className="ListImage__Image"
-                                />
-
-                                <button className="ListImage__Delete" onClick={() => this.deleteFile(file._id)}>Delete</button>
-                            </div>
-                        ))}
-                    </div>:null
-            }
-                    
-        </div>
-    )
+export default function WrittenResponse() {
+  const [code, setCode] = React.useState(
+    `function add(a, b) {\n  return a + b;\n}`
+  );
+  return (
+    <Editor
+      value={code}
+      onValueChange={(code) => setCode(code)}
+      highlight={(code) => highlight(code, languages.js)}
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontSize: 12,
+      }}
+    />
+  );
 }
--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const path = require('path')
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
-const crypto = require('crypto');
 require('dotenv').config()
 
 const app = express()
@@ -25,38 +22,12 @@ connection.once('open', () => {
     console.log('MongoDB connection established')
 })
 
-const storage = new GridFsStorage({
-    url: "mongodb+srv://pnav:pdatabase@cluster0.jvhte.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    options: {useNewUrlParser: true, useUnifiedTopology: true},
-    file: (req, file) => {
-        return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if (err) {
-                    return reject(err);
-                }
-                const filename = buf.toString('hex') + path.extname(file.originalname);
-                const fileInfo = {
-                    filename: filename,
-                    bucketName: 'uploads'
-                };
-                resolve(fileInfo);
-            });
-        });
-    }
-});
-
-const upload = multer({ storage });
-
-
-
 const userRouter = require('./routes/user')
-const imageRouter = require('./routes/image');
 const answerRouter = require('./routes/answer')
 const adminRouter = require('./routes/admin')
 app.use('/users',userRouter)
 app.use('/answer',answerRouter)
 app.use('/admin',adminRouter)
-app.use('/image', imageRouter(upload));
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
